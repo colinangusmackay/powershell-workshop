2779b3233272ad386231d8a10ea5170512040a7b
Colin Mackay <colin.mackay@arnoldclark.com>
2016-03-23 14:35:33 +0000

    Exercise example answer

---
+++ Ex-04-01-ExampleAnswer.ps1
@@ -0,0 +1,15 @@
+﻿function fibonacci([int]$n)
+{
+    if ($n -le 1) {
+        return $n;
+    }
+    $f1 = fibonacci ($n-1)
+    $f2 = fibonacci ($n-2)
+    return $f1 + $f2;
+}
+
+ForEach($n in 1..20)
+{
+    $f = fibonacci $n
+    Write-Host "F($n) = $f"
+}
