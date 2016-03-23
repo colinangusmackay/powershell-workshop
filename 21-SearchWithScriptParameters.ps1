c725b989ee15595f3b8439093e3721bc305bad65
Colin Mackay <colin.mackay@arnoldclark.com>
2016-03-23 14:38:59 +0000

    Search with script parameter

---
+++ 21-SearchWithScriptParameters.ps1
@@ -0,0 +1,12 @@
+﻿param
+(
+	[parameter(Mandatory=$true)]
+	[string] $File,
+	
+	[parameter(Mandatory=$true)]
+	[string] $SearchTerm
+)
+
+$content = Get-Content $File
+$count = ($content | ForEach-Object {$_ -match $SearchTerm} | Measure-Object -Sum).Sum
+Write-Host "'$SearchTerm' in $File was mentioned $count times."
