83dd6ade77c7e6e66dc4cf765a343e03f810279f
Colin Mackay <colin.mackay@arnoldclark.com>
2016-03-23 14:43:23 +0000

    Say Hello Module & Client

---
+++ 23-SayHelloClient.ps1
@@ -0,0 +1,5 @@
+﻿$moduleScript = "$PSScriptRoot\23-SayHelloModule.psm1"
+Write-Host $moduleScript
+Import-Module -Name $moduleScript -DisableNameChecking
+
+Say-Hello "Everyone"
---
+++ 23-SayHelloModule.psm1
@@ -0,0 +1,22 @@
+﻿Function Say-Hello
+{
+<#
+    .SYNOPSIS
+    Writes Hello to the console naming the subject of the salutation.
+
+    .DESCRIPTION
+    Writes Hello to the console naming the subject of the salutation. 
+    If no subject is given then it greets the world.
+
+    .PARAMETER Subject
+    The subject of the salutation. If missing will default to the World.
+#>
+    Param
+    (
+        [string]$Subject = "World"
+    )
+
+    Write-Host "Hello, $Subject"
+}
+
+Export-ModuleMember –Function Say-Hello 
