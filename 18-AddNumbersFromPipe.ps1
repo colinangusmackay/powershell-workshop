789625f4ee8786d4a3656e248999a5ef96e8f971
Colin Mackay <colin.mackay@arnoldclark.com>
2016-03-23 14:28:48 +0000

    Add Numbers from Pipe

---
+++ 18-AddNumbersFromPipe.ps1
@@ -0,0 +1,13 @@
+﻿function Add-Numbers()
+{
+    [int]$sum = 0
+    ForEach($value in $input)
+    {
+        $sum += $value
+    }
+    return $sum
+}
+
+$list = 1, 2, 3, 4, 5, 6, 7
+
+$list | Add-Numbers
