09d7d6f6c0841db7868ad8fc37e89185b89bdd74
Colin Mackay <colin.mackay@arnoldclark.com>
2016-03-23 14:07:09 +0000

    Error Action Stop

---
+++ 06-TryCatchFinallyErrorActionStop.ps1
@@ -0,0 +1,16 @@
+﻿Try
+{
+    Write-Host "In try block"
+    Get-Content from-a-file-that-does-not-exist.txt -ErrorAction Stop
+    Write-Host "This won't show because an exception has been thrown"
+}
+Catch
+{
+    Write-Host "In catch block"
+    Write-Host "The exception is placed int he variable `$_"
+    Write-Host $_
+}
+Finally
+{
+    Write-Host "This block always runs"
+}
