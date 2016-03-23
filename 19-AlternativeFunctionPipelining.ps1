d91e0133375289db075b27991f41394f36bf5b19
Colin Mackay <colin.mackay@arnoldclark.com>
2016-03-23 14:32:55 +0000

    Alternative Function Pipelining

---
+++ 19-AlternativeFunctionPipelining.ps1
@@ -0,0 +1,23 @@
+﻿Function Do-Something {
+	[CmdletBinding()]
+	param(
+		[Parameter(Mandatory=$True,
+		ValueFromPipeline=$True)]
+		[int[]]$values
+	)
+	BEGIN {
+        Write-Host "In the begin block : $values"
+    }
+	PROCESS {
+        Write-Host "In the process block : $values"
+    }
+	END {
+        Write-Host "In the end block : $values"
+    }
+}
+
+$a = 123,456,789
+
+$a | Do-Something
+
+Do-Something -values 123,456,789
