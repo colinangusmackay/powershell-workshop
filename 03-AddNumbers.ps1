1f3b219c8e8d98a5bc2ffa13768b76bcb6971089
Colin Mackay <colin.mackay@arnoldclark.com>
2016-03-23 14:00:56 +0000

    Add Numbers

---
+++ 03-AddNumbers.ps1
@@ -0,0 +1,8 @@
+﻿Write-Host Give me a number:
+$aString = Read-Host
+[int]$a = [Convert]::ToInt32($aString,10)
+Write-Host And another:
+$bString = Read-Host
+[int]$b = [Convert]::ToInt32($bString,10)
+$c = $a + $b
+Write-Host "The $a + $b = $c"
