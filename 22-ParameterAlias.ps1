e832da19d5802e6a79cda65474f91f0ff023696c
Colin Mackay <colin.mackay@arnoldclark.com>
2016-03-23 14:41:13 +0000

    Parameter Alias

---
+++ 22-ParameterAlias.ps1
@@ -0,0 +1,17 @@
+﻿param
+(
+    [parameter(Mandatory=$true)]
+    [Alias("FilePath")]
+    [Alias("Path")]
+    [Alias("f")]
+    [string] $File,
+	
+    [parameter(Mandatory=$true)]
+    [Alias("Search")]
+    [Alias("s")]
+    [string] $SearchTerm
+)
+
+$content = Get-Content $File
+$count = ($content | ForEach-Object {$_ -match $SearchTerm} | Measure-Object -Sum).Sum
+Write-Host "'$SearchTerm' in $File was mentioned $count times."
