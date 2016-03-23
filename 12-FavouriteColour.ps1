c60337f3bce0b431b57ef619356ff13a198d54ad
Colin Mackay <colin.mackay@arnoldclark.com>
2016-03-23 14:17:49 +0000

    Favourite Colours

---
+++ 12-FavouriteColour.ps1
@@ -0,0 +1,10 @@
+﻿Write-Host "What is your favourite colour?"
+$colour = Read-Host
+if ($colour -match "blue")
+{
+    Write-Host "I like something similar."
+}
+else
+{
+    Write-Host "Cool. I quite like blues."
+}
