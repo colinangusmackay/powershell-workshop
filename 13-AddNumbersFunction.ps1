48700c1f949165a8d7fca756257fc3e57817b05a
Colin Mackay <colin.mackay@arnoldclark.com>
2016-03-23 14:23:25 +0000

    Add Numbers

---
+++ 13-AddNumbersFunction.ps1
@@ -0,0 +1,7 @@
+﻿function Add-Numbers([int] $a, [int]$b)
+{
+    return $a+$b;
+}
+
+$answer = Add-Numbers 2 5
+Write-Host "2+5=$answer" 
---
+++ 14-AddNumbersFunctionBroken.ps1
@@ -0,0 +1,15 @@
+﻿function Add-Numbers
+{
+    Param
+    (
+        [parameter(Mandatory=$true)]
+        [int]$a,
+
+        [parameter(Mandatory=$true)]
+        [int]$b
+    )
+    return $a+$b
+}
+
+$answer = Add-Numbers 2, 5
+Write-Host "2+5=$answer" 
