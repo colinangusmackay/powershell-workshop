ee16536e4fc6668d8cdd4f56322f13fd2f61fcbc
Colin Mackay <colin.mackay@arnoldclark.com>
2016-03-23 14:14:10 +0000

    Exercise 2 example answers

---
+++ Ex-02-01-ExampleAnswer.ps1
@@ -0,0 +1,8 @@
+﻿$list = Get-ChildItem
+[int]$totalLength = 0
+foreach($item in $list)
+{
+    [int]$fileLength = $item.Length;
+    $totalLength = $totalLength + $fileLength
+}
+Write-Host "The total length of all files in this folder is $totalLength bytes"
---
+++ Ex-02-02-ExampleAnswer.ps1
@@ -0,0 +1,3 @@
+﻿$list = Get-ChildItem
+$totalLength = ($list | ForEach-Object {$_.Length} |  Measure-Object -Sum).Sum 
+Write-Host "The total length of all files in this folder is $totalLength bytes"
