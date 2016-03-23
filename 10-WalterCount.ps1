25ae0c881c40b87f632eab79377cc5a49e38f91c
Colin Mackay <colin.mackay@arnoldclark.com>
2016-03-23 14:11:25 +0000

    Walter Count

---
+++ 10-WalterCount.ps1
@@ -0,0 +1,3 @@
+﻿$content = Get-Content -path ".\data\sir-walter-scott.txt"
+$count = ($content | ForEach-Object {$_ -match "walter"} | Measure-Object -Sum).Sum
+Write-Host "'Walter' was mentioned $count times."
