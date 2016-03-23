99d11724c527c85dc4ba0d1f04d26e8088d9e89f
Colin Mackay <colin.mackay@arnoldclark.com>
2016-03-23 14:25:00 +0000

    Calculate Area

---
+++ 15-CalculateArea.ps1
@@ -0,0 +1,16 @@
+﻿function Calculate-Area([int]$Width, [int]$Height, [int]$Depth)
+{
+    $FrontFace = $Width * $Height
+    if ($Depth -eq $null){
+        return $FrontFace
+    }
+    $TopFace = $Width * $Depth
+    $SideFace = $Depth * $Height
+
+    return ($FrontFace+$TopFace+$SideFace)*2
+}
+
+$rectangle = Calculate-Area -Width 10 -Height 5
+Write-Host "The area of the rectangle is $rectangle"
+$cuboid = Calculate-Area -Depth 8 -Width 10 -Height 5
+Write-Host "The area of the cuboid is $cuboid"
