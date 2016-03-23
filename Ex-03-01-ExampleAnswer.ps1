19974fca5e89d14a5038dcc1939662ba04351fed
Colin Mackay <colin.mackay@arnoldclark.com>
2016-03-23 14:18:04 +0000

    Exercise 3 example answer

---
+++ Ex-03-01-ExampleAnswer.ps1
@@ -0,0 +1,12 @@
+﻿$files = Get-ChildItem
+ForEach($file in $files)
+{
+    if ($file.Length -gt 200)
+    {
+        Write-Host "BIG: $file"
+    }
+    else
+    {
+        Write-Host "SMALL: $file"
+    }
+}
